4134ac16b92d4b068a244daad510f7ff7e9a0b5d
Ifeoluwa Frank <ifeoluwa.frank@yahoo.com>
2024-01-31 15:25:32 +0100

    singlepost page

--- singlepost.php
+++ singlepost.php
@@ -73,49 +73,51 @@ if(isset($_GET['post_id']) && $_GET['post_id'] != ""){
 
             <?php foreach ($posts as $post) { ?>
                 <div class="each-post">
-                    <h3 class="aside"><?php echo $post['post_title'] ?></h3>
-                    <?php if($post['post_image'] != NULL){ ?>
-                        <img src="<?php echo $post['post_image']; ?>" height="400" width="100%"/>
-                    <?php } else {?>
-                        <img src="img/post-img.jpg" alt="post-image" height="150" width="600" />
-                    <?php } ?>
-                    <p><?php echo ($post['post_content']); ?></p>
-                    <?php if(isset($_SESSION['user_id'])) { ?>
-                        <?php if($post['post_author_id'] == $_SESSION['user_id']) {?>
-                            <div class="title-list">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.5"
-                                     stroke="currentColor"
-                                     class="delete-post-icon">
-                                    <path stroke-linecap="round"
-                                          stroke-linejoin="round"
-                                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                </svg>
-                                <a href="delete.php?post_id=<?php echo $post['post_id'] ?>" class="btnDelete">Delete Post</a>
-                            </div>
+                    <div class="each-post-post">
+                        <h3 class="aside"><?php echo $post['post_title'] ?></h3>
+                        <?php if($post['post_image'] != NULL){ ?>
+                            <img src="<?php echo $post['post_image']; ?>" height="400" width="100%"/>
+                        <?php } else {?>
+                            <img src="img/post-img.jpg" alt="post-image" height="150" width="600" />
                         <?php } ?>
-                    <?php } ?>
-                    <div class="author-home">
-                        <p><i>Author: <?php echo $post['firstname']. " " . $post['lastname']; ?></i></p>
-                        <div class="back">
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                fill="none" viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="delete-post-icon">
-                                <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                            </svg>
-                            <a href="index.php" class="btnHome">Home</a>
+                        <p><?php echo ($post['post_content']); ?></p>
+                        <?php if(isset($_SESSION['user_id'])) { ?>
+                            <?php if($post['post_author_id'] == $_SESSION['user_id']) {?>
+                                <div class="title-list">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5"
+                                         stroke="currentColor"
+                                         class="delete-post-icon">
+                                        <path stroke-linecap="round"
+                                              stroke-linejoin="round"
+                                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+                                    <a href="delete.php?post_id=<?php echo $post['post_id'] ?>" class="btnDelete">Delete Post</a>
+                                </div>
+                            <?php } ?>
+                        <?php } ?>
+                        <div class="author-home">
+                            <p><i>Author: <?php echo $post['firstname']. " " . $post['lastname']; ?></i></p>
+                            <div class="back">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    class="delete-post-icon">
+                                    <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
+                                </svg>
+                                <a href="index.php" class="btnHome">Home</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <form method="post">
                     <div class="comment-form">
-                        <textarea class="comment-field" rows="1" cols="50"></textarea>
+                        <textarea class="comment-field" rows="1" cols="50" placeholder="Add a comment..."></textarea>
                         <button class="comment-submit">Submit</button>
                     </div>
                 </form>
